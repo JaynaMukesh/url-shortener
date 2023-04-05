e979980c083b170f98d2f9517cb6ccec78f693f9
Jayna Mukesh <115871133+JaynaMukesh@users.noreply.github.com>
2023-04-05 10:31:17 +0000

    finishhh

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-
+import { FaLink } from "react-icons/fa"
 function App() {
   return (
-    <div className="">
-      
+    <div className="flex justify-center pt-16">
+      <div className='w-full'>
+        <div className='text-3xl font-semibold text-center'>MiniLinks</div>
+        <div className='pt-4 text-xl text-center'>MiniLinks is a free tool to shorten a URL.<br />Use our URL Shortener to create a rememberable and easy-to-share URL.</div>
+        <form className='my-12 mx-64 flex items-center gap-4 bg-white px-9 py-4 rounded-xl'>
+          <FaLink className='text-xl' />
+          <input type='url' placeholder='Enter the link here' className='py-3 w-full px-2 text-xl focus:outline-none ' />
+          <button className='bg-black text-white px-10 py-2 rounded-lg'>Minify</button>
+        </form>
+      </div>
     </div>
   );
 }
